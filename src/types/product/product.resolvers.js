ebb7b00f1aa110207c43c9e2d89c56c0cb3fa1b5
Stanley Jackson <stanley.jackson@sweetgreen.com>
2019-02-07 13:48:46 -0800

    product resolvers

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -9,10 +9,41 @@ const productsTypeMatcher = {
   DRONE: 'Drone'
 }
 
+const product = (_, args) => args.id
+
+const products = () => Product.find({}).exec()
+
+const newProduct = (_, args, ctx) =>
+  Product.create({ ...args.input, createdBy: ctx.user._id })
+
+const updateProduct = (_, args) =>
+  Product.findByIdAndUpdate(args.id, args.input, { new: true })
+    .lean() // Convert to JSON rather than Mongo document
+    .exec()
+
+const removeProduct = (_, args) =>
+  Product.findByIdAndRemove(args.id)
+    .lean()
+    .exec()
+
+// const createdBy = ({ createdBy }) => createdBy
+
 export default {
-  Query: {},
-  Mutation: {},
+  Query: {
+    product,
+    products
+  },
+  Mutation: {
+    newProduct,
+    updateProduct,
+    removeProduct
+  },
   Product: {
-    __resolveType(product) {}
+    __resolveType(product) {},
+    createdBy(product) {
+      return User.findById(product.createdBy)
+        .lean()
+        .exec()
+    }
   }
 }
